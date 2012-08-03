7d4f0ff81326df07658d6ef0f806143db5880174
milentije <mile_iz_back@hotmail.com>
2012-08-03 21:25:28 +0200

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Ruby on Rails vežbanje
+
+Vežbanje za Rails aplikaciju
+
+[*Ruby on Rails Tutorial*]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'cd257fabdc642cbdc62c61ca9dbc9db1fdb35e0564284ab3c0a4f8d1f70899f7d5e2d12e420962324785bdd3f32ec33a28e79658c0d23cff939f4cf5683143dc'
